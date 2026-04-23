1ca433719d0f96ac5a88e509a6bcae22c6655c0f
David Gaona <8864855@estudiantes.educeuta.es>
2026-04-23 19:01:34 +0200

    Implementada vistas (provisionales) de login y register y actualizado archivo HomeController para verificar

--- src/Controllers/HomeController.php
+++ src/Controllers/HomeController.php
@@ -2,11 +2,12 @@
 
 namespace App\Controllers;
 
+use App\Core\Request;
 use App\Core\View;
 
 class HomeController
 {
-    public function index()
+    public function index(Request $request): void
     {
         View::render('home', ['title' => 'SkillUP']);
     }
