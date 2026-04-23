44d9cf3a19951b775123d97565c36b6efd452c74
David Gaona <8864855@estudiantes.educeuta.es>
2026-04-23 20:56:05 +0200

    Solucionado otro problema de enrutamiento

--- src/routes.php
+++ src/routes.php
@@ -3,8 +3,8 @@
 $router->get('/', [\App\Controllers\HomeController::class, 'index']);
 $router->get('/cursos', [\App\Controllers\CursoController::class, 'index']);
 $router->get('/cursos/{id}', [\App\Controllers\CursoController::class, 'show']);
-$router->get('/login', [\App\Controllers\AuthController::class, 'loginForm']);
+$router->get('/login', [\App\Controllers\AuthController::class, 'formularioLogin']);
 $router->post('/login', [\App\Controllers\AuthController::class, 'login']);
-$router->get('/register', [\App\Controllers\AuthController::class, 'registerForm']);
+$router->get('/register', [\App\Controllers\AuthController::class, 'formularioRegister']);
 $router->post('/register', [\App\Controllers\AuthController::class, 'register']);
 $router->get('/logout', [\App\Controllers\AuthController::class, 'logout']);
