64bcb2a41603cbf87f8b7a6308342f47827a7b5a
David Gaona <8864855@estudiantes.educeuta.es>
2026-05-21 09:55:16 +0200

    Ahora los títulos de los apartados de admin sí se ven

--- src/Views/admin/layout.php
+++ src/Views/admin/layout.php
@@ -49,5 +49,7 @@
     <!-- jQuery y DataTables JS -->
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
     <script src="https://cdn.datatables.net/2.1.8/js/dataTables.min.js"></script>
+
+    <script src="/js/app.js"></script>
 </body>
 </html>
